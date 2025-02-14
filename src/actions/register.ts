6535ba11ceac604e063b8d76f4e87c63be43d67a
Полина <136325247+Ampersand04@users.noreply.github.com>
2025-02-14 17:19:17 +0300

    deploy: 1

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,21 +1,22 @@
-'use server';
+"use server";
 
-import { signIn } from '@/auth';
-import bcrypt from 'bcryptjs';
-import { userService } from '../service/user.service';
-import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { RegisterSchema } from '@/schemas';
-import { AuthError } from 'next-auth';
-import { z } from 'zod';
-import { error } from 'console';
-import { prisma } from '@/lib/prisma';
+import { signIn } from "@/auth";
+import bcrypt from "bcrypt";
+// import bcrypt from 'bcryptjs';
+import { userService } from "../service/user.service";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { RegisterSchema } from "@/schemas";
+import { AuthError } from "next-auth";
+import { z } from "zod";
+import { error } from "console";
+import { prisma } from "@/lib/prisma";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
     console.log(values);
 
     if (!validatedFields.success) {
-        return { error: 'Заполните поля!' };
+        return { error: "Заполните поля!" };
     }
 
     const { email, password, name } = validatedFields.data;
@@ -24,17 +25,17 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const existingUser = await userService.getUserByEmail(email);
 
     if (existingUser) {
-        console.log('Email уже используется');
-        return { error: 'Email уже используется' };
+        console.log("Email уже используется");
+        return { error: "Email уже используется" };
     }
 
     await prisma.user.create({
-        data: { name, email, password: hashedPassword, image: '' },
+        data: { name, email, password: hashedPassword, image: "" },
     });
-    console.log('Пользователь создан');
+    console.log("Пользователь создан");
 
     try {
-        await signIn('credentials', {
+        await signIn("credentials", {
             email,
             password,
             name,
@@ -44,14 +45,14 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     } catch (error) {
         if (error instanceof AuthError) {
             switch (error.type) {
-                case 'CredentialsSignin':
-                    return { error: 'Нет такого пользоваеля' };
+                case "CredentialsSignin":
+                    return { error: "Нет такого пользоваеля" };
                 default:
-                    return { error: 'Что-то пошло не так' };
+                    return { error: "Что-то пошло не так" };
             }
         }
 
         throw error;
     }
-    return { success: 'Пользователь создан' };
+    return { success: "Пользователь создан" };
 };
