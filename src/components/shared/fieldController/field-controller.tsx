7aac5466d515939ddb943c10d97065562dec51a2
Швороб Владислав <136325247+Ampersand04@users.noreply.github.com>
2026-05-28 19:22:55 +0300

    depliy: bugfix and refactoring

--- src/components/shared/fieldController/field-controller.tsx
+++ src/components/shared/fieldController/field-controller.tsx
@@ -1,32 +1,34 @@
-import React, { ReactElement } from 'react';
-import { Control, Controller } from 'react-hook-form';
-import * as rf from 'react-form';
+"use client";
+
+import React, { ReactElement } from "react";
+import { Control, Controller } from "react-hook-form";
+import * as rf from "react-form";
 
 interface FieldControllerProps {
-    className?: string;
-    control: Control<any>;
-    name: string;
-    rules?: any;
-    render: (fieldProps: { field: any; fieldState: any }) => ReactElement;
+  className?: string;
+  control: Control<any>;
+  name: string;
+  rules?: any;
+  render: (fieldProps: { field: any; fieldState: any }) => ReactElement;
 }
 
 const FieldController: React.FC<FieldControllerProps> = ({
-    className,
-    control,
-    name,
-    rules,
-    render,
+  className,
+  control,
+  name,
+  rules,
+  render,
 }) => {
-    return (
-        <div className={className}>
-            <Controller
-                name={name}
-                control={control}
-                rules={rules}
-                render={({ field, fieldState }) => render({ field, fieldState })}
-            />
-        </div>
-    );
+  return (
+    <div className={className}>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field, fieldState }) => render({ field, fieldState })}
+      />
+    </div>
+  );
 };
 
 export default FieldController;
