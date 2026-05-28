bfae6168fd856bfc535a788e8cf74e7a33ce156e
Швороб Владислав <136325247+Ampersand04@users.noreply.github.com>
2026-05-28 14:29:39 +0300

    updates-for-deploy

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,110 +1,111 @@
 // UsersPage.tsx
-'use client';
-import { useEffect, useState } from 'react';
-import { Table } from 'antd';
-import { User } from '@/components/dashboard/user.iterface';
-import DashboardPage from '@/components/dashboard/dashboard-page';
-import UserActionButtons from '@/components/dashboard/UserActionButtons';
-import { RoleGate } from '@/components/auth/role-gate';
-import { Role } from '@prisma/client';
-import dayjs from 'dayjs';
+"use client";
+import { useEffect, useState } from "react";
+import { Table } from "antd";
+import { User } from "@/components/dashboard/user.iterface";
+import DashboardPage from "@/components/dashboard/dashboard-page";
+import UserActionButtons from "@/components/dashboard/UserActionButtons";
+import { RoleGate } from "@/components/auth/role-gate";
+import { Role } from "@prisma/client";
+import dayjs from "dayjs";
 
 const UsersPage = () => {
-    const [dataSource, setDataSource] = useState<User[]>([]);
-    const [totalItems, setTotalItems] = useState(0);
+  const [dataSource, setDataSource] = useState<User[]>([]);
+  const [totalItems, setTotalItems] = useState(0);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch('/api/users');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch users');
-                }
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("/api/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
 
-                const data = await response.json();
-                const formattedData = data.map((user: any) => ({
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    premium: user.premium,
-                    createdAt: user.createdAt,
-                }));
+        const data = await response.json();
+        const formattedData = data.map((user: any) => ({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          premium: user.premium,
+          createdAt: user.createdAt,
+        }));
 
-                setDataSource(formattedData);
-                setTotalItems(data.length);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
-    }, []);
-
-    const handleDelete = (id: string) => {
-        setDataSource((prevData) => prevData.filter((item) => item.id !== id));
-        setTotalItems((prevTotal) => prevTotal - 1); // Обновляем общее количество
+        setDataSource(formattedData);
+        setTotalItems(data.length);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
     };
 
-    const columns = [
-        {
-            title: 'Имя',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Email',
-            dataIndex: 'email',
-            key: 'email',
-        },
-        {
-            title: 'Premium',
-            dataIndex: 'premium',
-            key: 'premium',
-            render: (premium: boolean) => (
-                <span className={premium ? 'text-green-500' : 'text-red-500'}>
-                    {premium ? 'Активный' : 'Не активен'}
-                </span>
-            ),
-        },
-        {
-            title: 'Дата создания',
-            dataIndex: 'createdAt',
-            key: 'createdAt',
-            render: (createdAt: Date | string): JSX.Element => (
-                <p>{dayjs(createdAt).format('DD.MM.YYYY')}</p>
-            ),
-        },
-        {
-            title: '',
-            key: 'actions',
-            fixed: 'right' as const,
-            render: (record: User) => (
-                <UserActionButtons objectId={record.id} onDelete={handleDelete} />
-            ),
-        },
-    ];
+    fetchUsers();
+  }, []);
 
-    return (
-        <RoleGate allowedRole={Role.ADMIN}>
-            <DashboardPage
-                pageName="Пользователи"
-                tableData={dataSource}
-                createLink="/dashboard/users/create">
-                <div className="w-full overflow-auto bg-admin-bg">
-                    <Table
-                        columns={columns}
-                        dataSource={dataSource}
-                        rowKey="id"
-                        pagination={{ total: totalItems }}
-                        scroll={{
-                            x: 'max-content',
-                            y: 70 * 5,
-                        }}
-                    />
-                </div>
-            </DashboardPage>
-        </RoleGate>
-    );
+  const handleDelete = (id: string) => {
+    setDataSource((prevData) => prevData.filter((item) => item.id !== id));
+    setTotalItems((prevTotal) => prevTotal - 1); // Обновляем общее количество
+  };
+
+  const columns = [
+    {
+      title: "Имя",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Premium",
+      dataIndex: "premium",
+      key: "premium",
+      render: (premium: boolean) => (
+        <span className={premium ? "text-green-500" : "text-red-500"}>
+          {premium ? "Активный" : "Не активен"}
+        </span>
+      ),
+    },
+    {
+      title: "Дата создания",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt: Date | string): JSX.Element => (
+        <p>{dayjs(createdAt).format("DD.MM.YYYY")}</p>
+      ),
+    },
+    {
+      title: "",
+      key: "actions",
+      fixed: "right" as const,
+      render: (record: User) => (
+        <UserActionButtons objectId={record.id} onDelete={handleDelete} />
+      ),
+    },
+  ];
+
+  return (
+    <RoleGate allowedRole={Role.ADMIN}>
+      <DashboardPage
+        pageName="Пользователи"
+        tableData={dataSource}
+        createLink="/dashboard/users/create"
+      >
+        <div className="w-full overflow-auto bg-admin-bg">
+          <Table
+            columns={columns}
+            dataSource={dataSource}
+            rowKey="id"
+            pagination={{ total: totalItems }}
+            scroll={{
+              x: "max-content",
+              y: 70 * 5,
+            }}
+          />
+        </div>
+      </DashboardPage>
+    </RoleGate>
+  );
 };
 
 export default UsersPage;
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,53 +1,53 @@
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-// import bcrypt from 'bcryptjs';
+// import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
 import { userService } from "./service/user.service";
 
 export default {
-    providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-        Credentials({
-            async authorize(credentials) {
-                const validatedFields = LoginSchema.safeParse(credentials);
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
 
-                if (!validatedFields.success) {
-                    throw new Error("Invalid input data");
-                }
+        if (!validatedFields.success) {
+          throw new Error("Invalid input data");
+        }
 
-                const { email, password } = validatedFields.data;
+        const { email, password } = validatedFields.data;
 
-                try {
-                    // Fetch user from your database or service
-                    const user = await userService.getUserByEmail(email);
+        try {
+          // Fetch user from your database or service
+          const user = await userService.getUserByEmail(email);
 
-                    if (!user || !user.password) {
-                        throw new Error("No user found with the email");
-                    }
+          if (!user || !user.password) {
+            throw new Error("No user found with the email");
+          }
 
-                    // Compare password
-                    const passwordMatch = await bcrypt.compare(password, user.password);
+          // Compare password
+          const passwordMatch = await bcrypt.compare(password, user.password);
 
-                    if (passwordMatch) {
-                        return user;
-                    }
+          if (passwordMatch) {
+            return user;
+          }
 
-                    return null;
-                } catch (error) {
-                    console.error(error);
-                    throw new Error("Authentication failed");
-                }
-            },
-        }),
-    ],
-    pages: {
-        signIn: "/auth/signin",
-        // signIn: '/auth/login',
-        // другие страницы
-    },
+          return null;
+        } catch (error) {
+          console.error(error);
+          throw new Error("Authentication failed");
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    // signIn: '/auth/login',
+    // другие страницы
+  },
 } satisfies NextAuthConfig;
